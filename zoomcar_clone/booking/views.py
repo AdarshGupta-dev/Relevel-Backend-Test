bbb858303511834725fd8acb1bc0a6092cc1a176
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-22 22:14:05 +0530

    minor

--- zoomcar_clone/booking/views.py
+++ zoomcar_clone/booking/views.py
@@ -4,8 +4,6 @@ from rest_framework import generics
 
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from booking import serializers
-
 
 class BookingCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
