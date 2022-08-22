d6848280d0e907b495a072c756251e25f9dd47b7
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-22 19:42:33 +0530

    minor changes

--- zoomcar_clone/py_client/booking/create.py
+++ zoomcar_clone/py_client/booking/create.py
@@ -2,13 +2,12 @@ import requests
 
 endpoint = "http://localhost:8000/booking/create/"
 
-Token = ""
+Token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjYxMTc2MzE1LCJpYXQiOjE2NjExNzU3MTUsImp0aSI6IjgxNTUwZWQzOTQ1MDRkNzViMmUzMGU0ZmI4N2Y4OTk2IiwidXNlcl9pZCI6Nn0.mjxXEtlH17ZZcqlJwqF7R89_Nzk8c-OTz_k8lYEWoso"
 
 data = {
     "vehicle": "maruti",
     "starting_date": "2022-12-24",
     "duration": "4",
-    "renter": "1",
 }
 
 response = requests.post(endpoint, data, headers={'Authorization': f'Bearer {Token}'})
